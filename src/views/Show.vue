902d08ca9d1503d0d954505078900fa5ecf50da7
wawanneutron <wawanneutron1331@gmail.com>
2021-09-22 00:36:43 +0700

    add to cart to databse

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -15,20 +15,29 @@
         <div class="card">
           <div class="card-body">
             <!-- card header -->
-            <transition name="slide-fade" mode="out-in">
+            <transition
+              v-if="gallery[photoActive]"
+              name="slide-fade"
+              mode="out-in"
+            >
               <img
-                src="https://adminsport.my.id/storage/product-images/t6EeU2RIKGE5ApMEG8HWt1G8X7mRa4K8Elbyv8A5.png"
+                :src="gallery[photoActive].image"
                 class="img-store img-fluid w-100"
               />
             </transition>
             <!-- thumbnail -->
-            <div class="row justify-content-center">
-              <div class="col-md-3 col-4 col-thumbnail">
-                <img
-                  class="w-100 img-thumbnail float-left"
-                  src="https://adminsport.my.id/storage/product-images/t6EeU2RIKGE5ApMEG8HWt1G8X7mRa4K8Elbyv8A5.png"
-                />
-              </div>
+            <div class="row justify-content-center mt-3">
+              <template v-for="(item, index) of gallery" :key="item.id">
+                <div class="col-md-3 col-4 col-thumbnail">
+                  <main @click="changeActive(index)">
+                    <img
+                      class="w-100 img-thumbnail float-left"
+                      :src="item.image"
+                      :class="{ active: index == this.photoActive }"
+                    />
+                  </main>
+                </div>
+              </template>
             </div>
           </div>
         </div>
@@ -37,7 +46,7 @@
         <div class="card border-0 rounded shadow">
           <div class="card-body">
             <label class="font-weight-bold" style="font-size: 20px">
-              Macbook Pro M1 Chipset
+              {{ product.title }}
             </label>
             <hr />
             <div
@@ -45,8 +54,12 @@
               id="price-product"
               style="font-size: 1.35rem"
             >
-              <span class="font-weight-bold mr-4">Rp. 18.000.000</span>
-              <s class="font-weight-bold">Rp. 21.000.000</s>
+              <span class="font-weight-bold me-4"
+                >Rp. {{ moneyFormat(calculateDiscount(product)) }}</span
+              >
+              <s class="font-weight-bold"
+                >Rp. {{ moneyFormat(product.price) }}</s
+              >
             </div>
             <table class="table table-show table-borderless mt-3">
               <tbody>
@@ -58,7 +71,7 @@
                       class="btn btn-sm"
                       style="color: #ff2f00; border-color: #ff2f00"
                     >
-                      DISKON 15 %
+                      DISKON {{ product.discount }} %
                     </button>
                   </td>
                 </tr>
@@ -74,7 +87,7 @@
                         padding: 0.25em 0.5em 0.2em;
                       "
                     >
-                      1500 gram</span
+                      {{ product.weight }} gram</span
                     >
                   </td>
                 </tr>
@@ -90,14 +103,23 @@
                         padding: 0.25em 0.5em 0.2em;
                       "
                     >
-                      23 stock</span
+                      {{ product.stock }} stock</span
                     >
                   </td>
                 </tr>
               </tbody>
             </table>
             <div>
-              <button class="btn btn-orange btn-lg btn-block">
+              <button
+                @click="
+                  addToCart(
+                    calculateDiscount(product),
+                    product.id,
+                    product.weight
+                  )
+                "
+                class="btn btn-orange btn-lg btn-block"
+              >
                 <i class="fa fa-shopping-cart"></i> TAMBAH KE KERANJANG
               </button>
             </div>
@@ -110,15 +132,60 @@
         <div class="card border-0 rounded shadow">
           <div class="card-body">
             <h3>Detail product</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim
-              doloremque nisi consequatur inventore delectus dolorem corporis
-              sunt. Nesciunt maiores asperiores explicabo, doloremque id nam
-              perferendis tempore dolor sequi nihil sit!
-            </p>
+            <p v-html="product.content"></p>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+import { computed, onMounted } from "@vue/runtime-core";
+import { useStore } from "vuex";
+import { useRoute, useRouter } from "vue-router";
+export default {
+  setup() {
+    const store = useStore();
+    const route = useRoute();
+    const router = useRouter();
+
+    onMounted(() => {
+      store.dispatch("product/detailProduct", route.params.slug);
+    });
+
+    const product = computed(() => {
+      return store.getters["product/getDetailProduct"];
+    });
+    const gallery = computed(() => {
+      return store.getters["product/getGalleries"];
+    });
+
+    const addToCart = (price, id, weight) => {
+      const token = store.state.auth.token;
+      if (!token) {
+        return router.push({ name: "login" });
+      }
+      store.dispatch("cart/addToCart", {
+        price: price,
+        product_id: id,
+        weight: weight,
+        quantity: 1,
+      });
+    };
+
+    return {
+      product,
+      gallery,
+      addToCart,
+    };
+  },
+  data: () => ({
+    photoActive: 0,
+  }),
+  methods: {
+    changeActive(id) {
+      this.photoActive = id;
+    },
+  },
+};
+</script>
